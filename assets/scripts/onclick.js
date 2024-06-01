81ff9993cafe4ed10c3a86569fe3cb1530b7894e
bearcubbb <74052670+bearcubbb@users.noreply.github.com>
2024-06-01 08:54:33 +0400

    Update onclick.js

--- assets/scripts/onclick.js
+++ assets/scripts/onclick.js
@@ -1,8 +1,9 @@
+const outputs = document.getElementsByClassName('output');
+
 function calculateTotalPrice(quantity = 2, price = 15000000) {
-    result = (quantity * price).toLocaleString('ru-RU');
+    const result = (quantity * price).toLocaleString('ru-RU');
 
     alert(`Стоимость покупки ${result} рублей`);
-    const outputs = document.getElementsByClassName('output');
     for (let i = 0; i < outputs.length; i++) {
         outputs[i].innerText = `Стоимость покупки ${result} рублей`;
         outputs[i].style.display = 'block';
